95b08989f212488268513f5bf902f2be8064b1b6
copenhagen2 <copenhagen@sjtu.edu.cn>
2022-10-01 01:17:34 +0800

    NONE
    UPDATE

--- scan.py
+++ scan.py
@@ -7,8 +7,7 @@ import os
 def img2bin(f_in, f_out):
     img = cv.imread(f_in)
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-    ret, thresh = cv.threshold(gray, 127, 255, cv.THRESH_BINARY)
-    cv.imwrite(f_out,thresh)
+    cv.imwrite(f_out, img)
 
 pdf = fpdf.FPDF()
 pdf.set_auto_page_break(0)
@@ -17,7 +16,7 @@ l = sorted(glob.glob(r'.\*.jpg'))
 for f in l:
     img2bin(f, f[:-4]+'_out.jpg')
     pdf.add_page()
-    pdf.image(f[:-4]+'_out.jpg', w=210, h=297)
+    pdf.image(f[:-4]+'_out.jpg', w=205, h=297)
     os.remove(f[:-4]+'_out.jpg')
 
 pdf.output(r'.\hw.pdf')   
